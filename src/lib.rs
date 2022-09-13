1ba0fd65cb76aadc07fa87ed5c2d861f92fb141c
Claudia Richoux <c@laudiacay.cool>
2022-09-13 01:14:56 -0400

    getting it passing tests so codecov will run maybe?

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,3 @@ pub mod eth;
 pub mod ipfs;
 pub mod proofs;
 pub mod types;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        unimplemented!("you need to write tests...");
-    }
-}
