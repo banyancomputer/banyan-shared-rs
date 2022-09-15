98858104107c8093af1e473309dae9627695485c
jonahkaye <jonah@banyan.computer>
2022-09-15 22:49:09 +0300

    TODOs finished and a lot cleaner

--- src/eth.rs
+++ src/eth.rs
@@ -5,15 +5,20 @@ use ethers::{
     contract::Contract,
     prelude::H256,
     providers::{Http, Middleware, Provider},
-    types::{Address, Filter, Log, U256},
+    types::{Address, Filter, U256},
 };
 
 use cid::Cid;
 use serde::{Deserialize, Serialize};
 use std::{convert::TryFrom, fs, str::FromStr};
 
+use std::{
+    io::{Cursor, Read},
+    ops::{Add, Mul},
+};
 use tokio::sync::Mutex;
-use std::ops::{Add, Mul};
+
+const WORD: usize = 32;
 
 #[derive(Serialize, Deserialize, Clone, Copy, Debug, PartialEq, Eq)]
 pub enum ProofBuddyMessageType {
@@ -53,17 +58,13 @@ impl VitalikProvider {
         })
     }
 
-    pub async fn get_contract_address(&self) -> Result<Address> {
+    pub async fn get_proof_from_logs(
+        &self,
+        submitted_proof_in_block_num: BlockNum,
+        deal_id: DealID,
+    ) -> Result<Option<Vec<u8>>> {
         let contract = self.contract.lock().await;
-        Ok(contract.address())
-    }
-
-    pub async fn get_proof_from_logs(&self, submitted_proof_in_block_num: BlockNum, deal_id: BlockNum) -> Vec<Log> 
-    {
-        let address = self
-        .get_contract_address()
-        .await
-        .map_err(|e| format!("Could not get contract address: {e}"))?;
+        let address = contract.address();
 
         let filter = Filter::new()
             .select(submitted_proof_in_block_num.0)
@@ -71,12 +72,32 @@ impl VitalikProvider {
             .address(address)
             .topic1(H256::from_low_u64_be(deal_id.0));
 
-        let block_logs = self.get_logs_from_filter(&filter).await.map_err(|e| {
-            format!(
-                "Couldn't get log from block {}: {}",
-                submitted_proof_in_block_num.0, e
-            )
-        })?;
+        let provider = self.provider.lock().await;
+        let block_logs = provider.get_logs(&filter).await?;
+
+        // The first two 32 byte words of log data are a pointer and the size of the data.
+        // TODO put this in banyan_shared!
+        let data = &block_logs[0].data;
+        if data.len() < WORD * 2 {
+            return Ok(None); // Err(format!("Data is too short: {:?}", data));
+        }
+        // TODO This works probably as a solution but might be a bug. I could submit a really long block,
+        // but since only takes last 8 bytes, it could slice the length value such that it read the
+        // length as the actual size. That would cause it to only read the first x bytes of the proof,
+        // and then it could read an incorrect proof as correct. BUTT the proof would have to have to
+        // be incorrect in such a way that it had the correct proof in the first x bytes, and then other
+        // incorrect stuff after, which means someone would have had to construct an correct proof and then
+        // decide to append other stuff for some reason. I don't know how that would help an attacker.
+        // Is this a bug? I am not sure.
+
+        let mut a = [0u8; 8];
+        a.clone_from_slice(&data[(WORD * 2) - 8..WORD * 2]);
+        let data_size = u64::from_be_bytes({ a });
+        if data.len() < WORD * 2 + data_size as usize {
+            return Ok(None);
+        }
+        let data_bytes: Vec<u8> = (&data[WORD * 2..WORD * 2 + data_size as usize]).to_vec();
+        Ok(Some(data_bytes))
     }
 
     pub async fn get_latest_block_num(&self) -> Result<BlockNum> {
@@ -93,20 +114,6 @@ impl VitalikProvider {
             .ok_or_else(|| anyhow!("block not found"))?;
         block.hash.ok_or_else(|| anyhow!("block hash not found"))
     }
-
-    pub async fn get_logs_from_filter(&self, filter: &Filter) -> Result<Vec<Log>> {
-        let provider = self.provider.lock().await;
-        let logs = provider.get_logs(filter).await?;
-        Ok(logs)
-    }
-
-    /* 
-    pub async fn get_logs_alternate(&self, filter: &Filter) -> Result<Vec<Log>> {
-        let provider = self.provider.lock().await;
-        let data_bytes = data.get(64..end);
-        Ok(logs)
-    }
-    */
 
     pub async fn get_proof_block_num_from_window(
         &self,
@@ -162,7 +169,7 @@ impl VitalikProvider {
                 .await?
                 .as_u64(),
         );
-        
+
         let collateral: TokenAmount = TokenAmount(
             contract
                 .method::<_, U256>("getCollateral", offer_id)?
@@ -170,21 +177,21 @@ impl VitalikProvider {
                 .await?
                 .as_u64(),
         );
-        
+
         let erc20_token_denomination: Token = Token(
             contract
                 .method::<_, Address>("getErc20TokenDenomination", offer_id)?
                 .call()
                 .await?,
         );
-        
+
         let cid_return: String = contract
             .method::<_, String>("getIpfsFileCid", offer_id)?
             .call()
             .await?;
 
         let ipfs_file_cid = Cid::from_str(cid_return.as_str())?;
-        
+
         let file_size: u64 = contract
             .method::<_, U256>("getFileSize", offer_id)?
             .call()
@@ -196,7 +203,7 @@ impl VitalikProvider {
             .call()
             .await?;
         let blake3_checksum = bao::Hash::from_str(&blake3_return)?;
-        
+
         let deal_info: OnChainDealInfo = OnChainDealInfo {
             deal_id,
             deal_start_block,
@@ -213,9 +220,33 @@ impl VitalikProvider {
         Ok(deal_info)
     }
 
-    pub fn compute_target_window_start(deal_start_block: BlockNum, proof_frequency_in_blocks: BlockNum, window_num: usize) -> BlockNum {
-        Add::add(Mul::mul(proof_frequency_in_blocks,window_num), deal_start_block)
+    pub fn compute_target_window_start(
+        deal_start_block: BlockNum,
+        proof_frequency_in_blocks: BlockNum,
+        window_num: usize,
+    ) -> BlockNum {
+        Add::add(
+            Mul::mul(proof_frequency_in_blocks, window_num),
+            deal_start_block,
+        )
     }
+
+    pub fn check_if_merkle_proof_is_valid(
+        proof_bytes: Cursor<&Vec<u8>>,
+        blake3_checksum: bao::Hash,
+        chunk_offset: u64,
+        chunk_size: u64,
+    ) -> Result<bool> {
+        if bao::decode::SliceDecoder::new(proof_bytes, &(blake3_checksum), chunk_offset, chunk_size)
+            .read_to_end(&mut vec![])
+            .is_ok()
+        {
+            return Ok(true);
+        } else {
+            return Ok(false);
+        };
+    }
+
     // the validator should be able to handle if proofs get sent twice on accident
     // return the block number that the proof made it into.
     pub async fn post_proof(&self, _deal_id: &DealID, _proof: Proof) -> Result<BlockNum> {
@@ -227,16 +258,13 @@ impl VitalikProvider {
     }
 }
 
-
-
 // tests
 #[cfg(test)]
 mod tests {
     use super::*;
     #[tokio::test]
 
-    async fn on_chain () ->  Result<(), anyhow::Error> 
-    {
+    async fn on_chain() -> Result<(), anyhow::Error> {
         let deal_id = DealID(1);
         let api_url = std::env::var("URL").expect("URL must be set.");
         let api_key = std::env::var("API_KEY").expect("API_KEY must be set.");
@@ -245,11 +273,10 @@ mod tests {
             std::env::var("CONTRACT_ADDRESS").expect("CONTRACT_ADDRESS must be set.");
         let provider = VitalikProvider::new(url, contract_address)
             .map_err(|e| format!("error with creating provider: {e}"))?;
-    
+
         let deal_info = provider
             .get_onchain(deal_id)
             .await
             .map_err(|e| format!("Error in get_onchain: {:?}", e))?;
-        
     }
 }
